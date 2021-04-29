bc849e0f31ccce04144b154afaea516cc1884182
STNyame <sentwalidors@hotmail.com>
2021-04-29 16:00:19 +0200

    skeleton movie database

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <p>This is the AboutPage</p>
+    </div>
+  );
+}
---
+++ src/pages/DiscoverMoviesPage.js
@@ -0,0 +1,7 @@
+export default function DiscoverMoviesPage() {
+  return (
+    <div>
+      <p>This is the DiscoverMoviesPage</p>
+    </div>
+  );
+}
